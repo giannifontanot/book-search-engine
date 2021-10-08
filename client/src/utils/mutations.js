ba158b5677cbc56297371d6e5f07f06a41245c1b
giannifontanot <giannifontanot@gmail.com>
2021-10-08 11:07:37 -0500

    Added skeletons of queries and mutations to the client.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,21 @@
+import {gql} from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser(){
+        
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook(){
+
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook(){
+
+    }
+`;
+
+
